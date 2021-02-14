c4ca91a1859a9d7003035cb67690f20988eb5ae8
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2021-02-14 15:29:54 +0530

    updated projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,6 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   titleString = 'My Projects';
+  myProjects = [
+    {
+      projectName:"Hate Speech Project",
+      createdTime:"2020-01-20 3:15PM",
+      trainingStarted:"2020-01-20 3:15PM",
+      trainingStatus:1,
+      projectType:"Analytics",
+      regression:"Prediction"
+    },
+    {
+      projectName:"Hate Speech Project",
+      createdTime:"2020-01-20 3:15PM",
+      trainingStarted:"2020-01-20 3:15PM",
+      trainingStatus:0,
+      projectType:"Analytics",
+      regression:"Prediction"
+    },
+    {
+      projectName:"Hate Speech Project",
+      createdTime:"2020-01-20 3:15PM",
+      trainingStarted:"2020-01-20 3:15PM",
+      trainingStatus:1,
+      projectType:"Analytics",
+      regression:"Prediction"
+    },
+    {
+      projectName:"Hate Speech Project",
+      createdTime:"2020-01-20 3:15PM",
+      trainingStarted:"2020-01-20 3:15PM",
+      trainingStatus:0,
+      projectType:"Analytics",
+      regression:"Prediction"
+    }
+  ];
 
   constructor() { }
 
